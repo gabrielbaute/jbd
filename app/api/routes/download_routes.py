39a30630b4339c22dc16b756d100551118364ff5
gabrielbaute <gabrielbaute@gmail.com>
2026-04-01 16:49:06 -0400

    Ajustamos la ruta de download para que responda a peticiones al socket

--- app/api/routes/download_routes.py
+++ app/api/routes/download_routes.py
@@ -4,7 +4,7 @@ Rutas para la gestión de descargas de álbumes.
 from uuid import UUID
 from typing import Optional
 from pydantic import BaseModel
-from fastapi import APIRouter, Depends, BackgroundTasks
+from fastapi import APIRouter, BackgroundTasks, WebSocket, WebSocketDisconnect, Depends
 
 from app.enums import Format, Bitrate
 from app.managers import DownloadManagerAPI
@@ -37,3 +37,33 @@ async def start_download(
         "job_id": request.job_id,
         "message": "Descarga enviada a la cola de procesamiento."
     }
+
+@router.websocket("/ws/progress/{job_id}")
+async def websocket_progress_endpoint(
+    websocket: WebSocket,
+    job_id: UUID,
+    manager: DownloadManagerAPI = Depends(get_download_api_manager)
+) -> None:
+    """
+    Endpoint de WebSocket para el seguimiento en tiempo real del progreso de descarga.
+
+    Args:
+        websocket (WebSocket): Instancia de la conexión WebSocket.
+        job_id (UUID): Identificador único del trabajo (generado por el frontend).
+        manager (DownloadManagerAPI): Manager encargado de la orquestación y sockets.
+
+    Returns:
+        None
+    """
+    # El manager acepta la conexión y la guarda en su diccionario interno
+    await manager.register_connection(job_id, websocket)
+    
+    try:
+        while True:
+            # Mantenemos la conexión abierta. 
+            # El cliente puede enviar pings para evitar timeouts de red.
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        # La limpieza final del socket la hace el manager en el bloque 'finally'
+        # de la tarea en segundo plano (download_album_job).
+        pass
