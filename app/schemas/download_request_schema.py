2d67641cec237fc994fc194c05ccf20af04e303f
gabrielbaute <gabrielbaute@gmail.com>
2026-04-01 13:45:03 -0400

    Implementamos un esquema DownloadRequest para que la API pueda pedir un estado específico de descargas

--- app/schemas/download_request_schema.py
+++ app/schemas/download_request_schema.py
@@ -1,20 +1,23 @@
+from uuid import UUID
 from pydantic import BaseModel
 from typing import List, Optional
 
 from app.enums import Format, Bitrate
-from app.schemas import AlbumResponse
+from app.schemas.album_response import AlbumResponse
 
 class DownloadRequest(BaseModel):
     """
     Objeto para solicitar la descarga de un álbum filtrado.
 
     Attributes:
+        job_id (UUID): Identificador único de la solicitud de descarga.
         album_data (AlbumResponse): Datos del álbum filtrado. Contiene los tracks ya filtrados/seleccionados por el frontend
         genre (Optional[str]): Género musical para asignar a los tracks del album.
         bitrate (Bitrate): Bitrate para asignar a los tracks del album. Por defecto es B_128K (128kbps).
         format (Format): Formato de descarga de los tracks del album. Por defecto es MP3.
     """
+    job_id: UUID
     album_data: AlbumResponse
-    genre: Optional[str] = None
-    bitrate: Bitrate = Bitrate.B_128K
     format: Format = Format.MP3
+    bitrate: Bitrate = Bitrate.B_128K
+    genre: Optional[str] = None    
